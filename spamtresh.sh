ae5fdc71a505219bf02dcee88ba1c6bf7597e650
fredtz <farkhad.badalov@gmail.com>
2016-11-02 09:12:32 +0400

    Update spamtresh.sh

--- spamtresh.sh
+++ spamtresh.sh
@@ -5,7 +5,7 @@ res=$(/usr/sbin/exim -bpc)
 if [ $res -ge "500" ];
 then
         echo "yes"
-mail -s "NS1 Exim mail queue warning more than 500" "farhad@mikrotex.biz" <<EOF
+mail -s "host1 Exim mail queue warning more than 500" "support@mail" <<EOF
 EXIM MAIL QUEUE WARNING
 EOF
 fi 
